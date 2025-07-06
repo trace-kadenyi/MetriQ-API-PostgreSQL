b1e773fdef9dd06c382ef76953861df5f6118925
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-06 17:38:24 +0300

    Delete unused comments

--- routes/urlChecker.js
+++ routes/urlChecker.js
@@ -1,36 +1,3 @@
-// const express = require("express");
-// const axios = require("axios");
-// const router = express.Router();
-
-// router.post("/check", async (req, res) => {
-//   const { url } = req.body;
-
-//   if (!url) {
-//     return res
-//       .status(400)
-//       .json({ success: false, message: "No URL provided." });
-//   }
-
-//   try {
-//     new URL(url);
-//   } catch {
-//     return res
-//       .status(400)
-//       .json({ success: false, message: "Invalid URL format." });
-//   }
-
-//   try {
-//     const response = await axios.get(url, { timeout: 5000 });
-//     return res.json({ success: true, status: response.status });
-//   } catch (err) {
-//     return res.status(400).json({
-//       success: false,
-//       message: "URL is unreachable or does not exist.",
-//     });
-//   }
-// });
-
-// module.exports = router;
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
