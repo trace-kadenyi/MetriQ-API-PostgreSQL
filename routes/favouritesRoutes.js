7e26cb94b6beab7f6fde031fd4945b543adc97ed
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-08 22:51:16 +0300

    Add claim favourites route

--- routes/favouritesRoutes.js
+++ routes/favouritesRoutes.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const {
   getFavourites,
   toggleFavourite,
+  claimFavourites,
 } = require("../controllers/favouriteController");
 
 // get route
 router.get("/", getFavourites);
 // post route
 router.post("/toggle", toggleFavourite);
+//handle transfer from anon to user
+router.post("/claim", claimFavourites);
 
 module.exports = router;
