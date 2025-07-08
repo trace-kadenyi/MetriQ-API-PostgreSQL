50af50b0a01ae6e86ef4de57b8270a634fc6d6c0
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-08 14:16:45 +0300

    Ad owner

--- models/FavouriteSchema.js
+++ models/FavouriteSchema.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 // favourites schema
 const favouritesSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true, unique: true }, // 👈 important for anon-based tracking
+    /** Present if the visitor is logged‑in */
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+
+    /** Present if the visitor is anonymous (what you called userId before) */
+    anonId: { type: String, default: null },
+
     favourites: {
       type: [String],
       validate: [
@@ -20,5 +25,9 @@ const favouritesSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Make sure each account (or anon id) has **one** doc
+favouritesSchema.index({ owner: 1 }, { unique: true, sparse: true });
+favouritesSchema.index({ anonId: 1 }, { unique: true, sparse: true });
 
 module.exports = mongoose.model("Favourites", favouritesSchema);
