325f3faa9d8604763d93de2bfac2430e9c6482a6
Tracey Kadenyi <treykadenyi@gmail.com>
2026-05-06 17:51:03 +0300

    remove outdated file

--- src/utils/getUserId.js
+++
@@ -1,8 +0,0 @@
-const getUserId = (req) => {
-  if (req.user && req.isAuthenticated()) {
-    return req.user._id || req.user.id; // whichever is present
-  }
-  return req.header("x-anon-id") || null; // fallback to anon user
-};
-
-module.exports = { getUserId };
